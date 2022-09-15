ad0f3bee2f68034031ee1642f6dd1298f60c778d
sam <sammtcbn@gmail.com>
2022-09-16 01:54:39 +0800

    script for docker inspect

--- docker-apt-install.sh
+++ docker-apt-install.sh
@@ -5,7 +5,7 @@ sudo apt -y update || exit 1
 sudo apt -y install ca-certificates curl gnupg lsb-release || exit 1
 
 # Add Docker’s official GPG key
-sudo mkdir -p /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings || exit 1
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -17,9 +17,9 @@ sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin 
 # Install Docker Compose
 sudo apt -y install docker-compose || exit 1
 
-docker -v
+docker -v || exit 1
 
 sudo usermod -aG docker ${USER}
 
 # reload the user groups
-newgrp docker
+newgrp docker || exit 1
